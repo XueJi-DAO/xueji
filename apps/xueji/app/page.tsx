c80b31a7699cb0c505ccc1655677615c8f1709ba
aaron <563459375@qq.com>
2024-03-26 16:55:39 +0800

    chore: 升级nx、nextjs

--- apps/xueji/app/page.tsx
+++ apps/xueji/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
   const session = await auth() // 服务组件获取登录状态
   return (
     <>
-      <Image alt="" src="/images/1.jpg" height={60} width={60} className="object-cover aspect-square col-span-2" />
+      <Image alt="" src="/images/1.jpg" height={60} width={60} className="col-span-2 aspect-square object-cover" />
       <div className="border-primary mx-auto flex w-fit items-center space-x-2 rounded-lg border p-1.5">
         <h1>学记助理</h1>
       </div>
--- apps/xueji/next.config.js
+++ apps/xueji/next.config.js
@@ -23,6 +23,11 @@ const nextConfig = {
       topLevelAwait: true,
       asyncWebAssembly: true,
     }
+    /**
+     * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
+     * Module parse failed: Unexpected character '�' (1:0)" error
+     */
+    config.resolve.alias.canvas = false
     return config
   },
   nx: {
