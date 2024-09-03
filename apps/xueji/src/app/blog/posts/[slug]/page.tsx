1cd64ade304612d6a275cec8485d3233044409b8
aaron <563459375@qq.com>
2024-09-04 00:08:55 +0800

    chore: blog

--- apps/xueji/src/app/blog/posts/[slug]/page.tsx
+++ apps/xueji/src/app/blog/posts/[slug]/page.tsx
@@ -2,12 +2,13 @@ import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { getAllPosts, getPostBySlug } from '@/lib/blog/api'
 import { CMS_NAME } from '@/lib/constants'
-import markdownToHtml from '@/lib/blog/markdownToHtml'
+// import markdownToHtml from '@/lib/blog/markdownToHtml'
 import Alert from '../../_components/alert'
 import Container from '../../_components/container'
 import Header from '../../_components/header'
 import { PostBody } from '../../_components/post-body'
 import { PostHeader } from '../../_components/post-header'
+import { getPostWords, readingTime } from '@xueji/utils'
 
 type Params = {
   params: {
@@ -23,14 +24,20 @@ export default async function Post({ params }: Params) {
   }
 
   // const content = await markdownToHtml(post.content || '')
-
+  const words = getPostWords(post.content)
+  const readTime = readingTime(words)
   return (
     <main>
       <Alert preview={post.preview} />
       <Container>
         <Header />
         <article className="mb-32">
+          <div className="flex">
+            <p className="mt-2 text-[13px] text-gray-700 dark:text-gray-300">字数：{words}</p>
+            <p className="mt-2 text-[13px] text-gray-700 dark:text-gray-300">预计阅读时间：{readTime}分钟</p>
+          </div>
           <PostHeader title={post.title} coverImage={post.coverImage} date={post.date} author={post.author} />
+
           <PostBody content={post.content} />
         </article>
       </Container>
